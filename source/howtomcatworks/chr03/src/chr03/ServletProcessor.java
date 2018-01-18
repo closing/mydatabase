a0cf595697514e00b6d8d2267dfa7659e0144e99
closing <zhu.yuxing@hotmail.com>
2018-01-18 14:50:28 +0900

    Create ServletProcessor.java

---
+++ source/howtomcatworks/chr03/src/chr03/ServletProcessor.java
@@ -0,0 +1,9 @@
+package chr03;
+
+import chr03.connector.http.HttpRequest;
+import chr03.connector.http.HttpResponse;
+
+public class ServletProcessor {
+	public void process(HttpRequest request, HttpResponse response) {
+	}
+}
