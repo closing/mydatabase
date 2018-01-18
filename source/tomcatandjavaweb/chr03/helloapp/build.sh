295da59aeb685d64205b8d7b48585b451dfbf967
closing <zhu.yuxing@hotmail.com>
2018-01-18 21:45:15 +0900

    Update build.sh

--- source/tomcatandjavaweb/chr03/helloapp/build.sh
+++ source/tomcatandjavaweb/chr03/helloapp/build.sh
@@ -1,1 +1,2 @@
-javac -classpath "$CATALINA_HOME/lib/servlet-api.jar" -sourcepath src -d WEB-INF/classes src/mypack/DispatcherServlet.java 
+javac -classpath "$CATALINA_HOME/lib/servlet-api.jar:$CATALINA_HOME/lib/jsp-api.jar" -sourcepath src -d WEB-INF/classes src/mypack/DispatcherServlet.java javac -classpath "$CATALINA_HOME/lib/servlet-api.jar:$CATALINA_HOME/lib/jsp-api.jar" -sourcepath src -d WEB-INF/classes src/mypack/DispatcherServlet.java 
+
