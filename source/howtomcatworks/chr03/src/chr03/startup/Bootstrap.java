3e19a75dab8ba112110147b8b919830801d28d1b
closing <zhu.yuxing@hotmail.com>
2018-01-18 14:45:14 +0900

    Update Bootstrap.java

--- source/howtomcatworks/chr03/src/chr03/startup/Bootstrap.java
+++ source/howtomcatworks/chr03/src/chr03/startup/Bootstrap.java
@@ -1,1 +1,11 @@
+package chr03.startup;
 
+import chr03.connector.http.HttpConnector;
+
+public final class Bootstrap {
+	public static void main(String[] args) {
+		System.out.println("Bootstrap.main: Server starting...");
+		HttpConnector connector = new HttpConnector();
+		connector.start();
+	}
+}
