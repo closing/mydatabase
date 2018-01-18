0f0cbb399dca4cdcb7e1f4656469c941426061e3
closing <zhu.yuxing@hotmail.com>
2018-01-18 14:48:18 +0900

    Create HttpResponseFacade.java

---
+++ source/howtomcatworks/chr03/src/chr03/connector/http/HttpResponseFacade.java
@@ -0,0 +1,9 @@
+package chr03.connector.http;
+
+public class HttpResponseFacade {
+	private HttpResponse response;
+	public HttpResponseFacade(HttpResponse response) {
+		this.response = response;
+	}
+
+}
