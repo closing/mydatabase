6451368797643c35a4fd499f8207db1b1101c6d5
closing <zhu.yuxing@hotmail.com>
2018-01-24 17:44:26 +0900

    Create runnew.cmd

---
+++ source/howtomcatworks/chr10/runnew.cmd
@@ -0,0 +1,1 @@
+java -cp "classes;lib\commons-logging.jar;lib\commons-digester.jar;lib\servlet.jar;lib\catalina.jar;lib\naming-resources.jar;lib\bootstrap.jar;lib\naming-common.jar;lib\commons-collections.jar" chr10.startup.BootstrapNew
