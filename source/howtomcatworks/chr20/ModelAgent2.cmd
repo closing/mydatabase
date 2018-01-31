5dcb0e5af093bad5d2820b713c3a4456705e1e14
closing <zhu.yuxing@hotmail.com>
2018-01-31 16:34:09 +0900

    Create ModelAgent2.cmd

---
+++ source/howtomcatworks/chr20/ModelAgent2.cmd
@@ -0,0 +1,1 @@
+java -cp "classes;lib\mx4j-2.0.1.jar;lib\commons-modeler.jar;lib\commons-digester.jar;lib\commons-logging.jar;lib\commons-collections.jar;lib\commons-beanutils.jar" chr20.modelmbeantest2.ModelAgent
