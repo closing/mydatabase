ca5d838e6eedafffd7372c020ab6aea4ebc4a4e6
closing <zhu.yuxing@hotmail.com>
2018-01-31 16:34:38 +0900

    Create build.cmd

---
+++ source/howtomcatworks/chr20/build.cmd
@@ -0,0 +1,1 @@
+javac @sources.txt -sourcepath src -d classes -cp "classes;lib\commons-digester.jar;lib\mx4j-2.0.1.jar;lib\commons-modeler.jar;lib\servlet.jar;lib\commons-logging.jar;lib\commons-collections.jar;lib\commons-beanutils.jar"
