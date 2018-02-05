0a3c912b7d39cfa73523ce4e8b116bec0679f443
closing <zhu.yuxing@hotmail.com>
2018-02-05 09:25:19 +0900

    Create CGItest.py

---
+++ source/tomcatdefinitiveguide/chr02/cgitest/WEB-INF/cgi/CGItest.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python
+# Trivial CGI demo
+print ("contentType: text/html")
+print ("")
+print ("<html><head><title>Welcome</title></head>")
+print ("<body><h1>Welcome to the output of a CGI under Tomcat</h1>")
+print ("<p>The subject says all.</p>")
+print ("</body></html>")
