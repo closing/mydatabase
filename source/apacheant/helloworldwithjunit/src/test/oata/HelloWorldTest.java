178b9b43a85f40cd651a795b3b6e2a502dea58be
closing <zhu.yuxing@hotmail.com>
2018-02-09 13:49:59 +0900

    Create HelloWorldTest.java

---
+++ source/apacheant/helloworldwithjunit/src/test/oata/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package oata;
+
+import junit.framework.TestCase;
+
+public class HelloWorldTest extends TestCase {
+
+	public void testNothing() {
+	}
+	public void testWillAlwaysFail() {
+		fail("An error message!!!");
+	}
+}
