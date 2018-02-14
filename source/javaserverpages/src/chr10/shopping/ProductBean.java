ddfcf37f82327f534b18338ec9dc097f104a1165
closing <zhu.yuxing@hotmail.com>
2018-02-14 15:30:48 +0900

    Create ProductBean.java

---
+++ source/javaserverpages/src/chr10/shopping/ProductBean.java
@@ -0,0 +1,42 @@
+package chr10.shopping;
+
+import java.io.Serializable;
+
+public class ProductBean implements Serializable {
+  private String id;
+  private String name;
+  private String descr;
+  private float price;
+
+  public String getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getDescr() {
+    return descr;
+  }
+
+  public float getPrice() {
+    return price;
+  }
+
+  void setId(String id) {
+    this.id = id;
+  }
+
+  void setName(String name) {
+    this.name = name;
+  }
+
+  void setDescr(String descr) {
+    this.descr = descr;
+  }
+
+  void setPrice(float price) {
+    this.price = price;
+  }
+}
