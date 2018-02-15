370547a2326cfbb6d15b17a8fee2f3f4541fb06b
closing <zhu.yuxing@hotmail.com>
2018-02-15 14:09:03 +0900

    Create IfValidEmailAddrTag.java

---
+++ source/javaserverpages/src/common/tags/IfValidEmailAddrTag.java
@@ -0,0 +1,18 @@
+package common.tags;
+
+import javax.servlet.jsp.JspTagException;
+import javax.servlet.jsp.jstl.core.ConditionalTagSupport;
+
+import util.StringFormat;
+
+public class IfValidEmailAddrTag extends ConditionalTagSupport {
+  private String value;
+
+  public void setValue(String value) {
+    this.value = value;
+  }
+
+  public boolean condition() throws JspTagException {
+    return StringFormat.isValidEmailAddr(value);
+  }
+}
