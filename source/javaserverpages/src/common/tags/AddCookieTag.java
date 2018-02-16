a63feaf25237669ba83863e045f2d2bc91c3bbba
closing <zhu.yuxing@hotmail.com>
2018-02-16 10:11:24 +0900

    Create AddCookieTag.java

---
+++ source/javaserverpages/src/common/tags/AddCookieTag.java
@@ -0,0 +1,40 @@
+package common.tags;
+
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+import javax.servlet.jsp.JspTagException;
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.JspException;
+import javax.servlet.http.HttpServletResponse;
+import util.CookieUtils;
+
+public class AddCookieTag extends SimpleTagSupport {
+  private String name;
+  private String value;
+  private String maxAgeString;
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setValue(String value) {
+    this.value = value;
+  }
+
+  public void setMaxAge(String maxAgeString) {
+    this.maxAgeString = maxAgeString;
+  }
+
+  public void doTag() throws JspException {
+    int maxAge = -1;
+    if (maxAgeString != null) {
+      try {
+        maxAge = Integer.valueOf(maxAgeString).intValue();
+      } catch (NumberFormatException e) {
+        throw new JspTagException("Invalid maxAge", e);
+      }
+    }
+    PageContext pageContext = (PageContext) getJspContext();
+    HttpServletResponse response = (HttpServletResponse) pageContext.getResponse();
+    CookieUtils.sendCookie(name, value, maxAge, response);
+  }
+}
