92820cf9b49c065de9043ed8a258702719485b92
closing <zhu.yuxing@hotmail.com>
2018-02-19 09:43:12 +0900

    Create SetHeaderTag.java

---
+++ source/javaserverpages/src/common/tags/SetHeaderTag.java
@@ -0,0 +1,31 @@
+package common.tags;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.tagext.JspTagException;
+import javax.servlet.jsp.JspException;
+import org.apache.taglibs.standard.lang.support.SimpleTagSupport*;
+
+public class SetHeaderTag extends SimpleTagSupport {
+    private String name;
+    private String value;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public void doTag() throws JspException {
+        if (value.length() == 0 || name.length() == 0) {
+            throw new JspTagException("setHeader: " +
+                 "one of the attributes is not set");
+        }
+        PageContext pageContext = (PageContext) getJspContext();
+        HttpServletResponse response = 
+            (HttpServletResponse) pageContext.getResponse();
+        response.setHeader(name, value);
+    }
+}
