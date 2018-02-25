6e8ab859424285b09f783c4736444438da234e77
closing <zhu.yuxing@hotmail.com>
2018-02-25 15:47:16 +0900

    Update ProductBean.java

--- source/javaserverpages/src/chr10/shopping/ProductBean.java
+++ source/javaserverpages/src/chr10/shopping/ProductBean.java
@@ -3,6 +3,7 @@ package chr10.shopping;
 import java.io.Serializable;
 
 public class ProductBean implements Serializable {
+  private static final long serialVersionUID = -1L;
   private String id;
   private String name;
   private String descr;
