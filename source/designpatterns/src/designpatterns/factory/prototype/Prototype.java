7e4e9001d1795bef877b21218abaa8b590194e44
closing <zhu.yuxing@hotmail.com>
2018-04-03 09:17:32 +0900

    Create Prototype.java

---
+++ source/designpatterns/src/designpatterns/factory/prototype/Prototype.java
@@ -0,0 +1,47 @@
+package designpatterns.prototype;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class Prototype implements Serializable, Cloneable {
+	private static final long serialVersionUID = 1L;
+	private String string;
+
+	private SerializableObject obj;
+
+	public Object clone() throws CloneNotSupportedException {
+		Prototype proto = (Prototype) super.clone();
+		return proto;
+	}
+
+	public Object deepClone() throws IOException, ClassNotFoundException {
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		ObjectOutputStream oos = new ObjectOutputStream(bos);
+		oos.writeObject(this);
+
+		ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());
+		ObjectInputStream ois = new ObjectInputStream(bis);
+		return ois.readObject();
+	}
+
+	public String getString() {
+		return string;
+	}
+
+	public void setString(String string) {
+		this.string = string;
+	}
+
+	public SerializableObject getObj() {
+		return obj;
+	}
+
+	public void setObj(SerializableObject obj) {
+		this.obj = obj;
+	}
+
+}
