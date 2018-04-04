e8c8db30890f52eeff22e3b3c976512a17b4a864
closing <zhu.yuxing@hotmail.com>
2018-04-04 09:17:14 +0900

    Create BuildFromXmlTest.java

---
+++ source/MyBatisTutorial/src/test/java/org/mybatis/example/BuildFromXmlTest.java
@@ -0,0 +1,38 @@
+package org.mybatis.example;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class BuildFromXmlTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public BuildFromXmlTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( BuildFromXmlTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
