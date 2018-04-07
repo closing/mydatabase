338645af632728610aef881682eabd12d44b587b
closing <zhu.yuxing@hotmail.com>
2018-04-07 17:44:12 +0900

    Update PrototypeTest.java

--- source/designpatterns/src/designpatterns/client/PrototypeTest.java
+++ source/designpatterns/src/designpatterns/client/PrototypeTest.java
@@ -1,32 +1,33 @@
+
 package designpatterns.client;
 
 import java.io.IOException;
 
-import designpatterns.prototype.Prototype;
+import designpatterns.prototype.ProtoType;
 import designpatterns.prototype.SerializableObject;
 
 public class PrototypeTest {
 	public static void main(String args[]) throws CloneNotSupportedException, ClassNotFoundException, IOException {
-		Prototype source = new Prototype();
-		source.setString("source");
+		ProtoType source = new ProtoType();
+		source.setStr("source");
 		source.setObj(new SerializableObject());
 		System.out.println("----------------------------------------");
 		System.out.println("Class ID of source: " + source.toString());
-		System.out.println("--String of source: " + source.getString());
+		System.out.println("--String of source: " + source.getStr());
 		System.out.println("--String of obj: " + source.getObj().toString());
 		System.out.println("----------------------------------------");
 
-		Prototype target = (Prototype) source.clone();
+		ProtoType target = (ProtoType) source.clone();
 		System.out.println("Clone:");
 		System.out.println("Class ID of target: " + target.toString());
-		System.out.println("--String of target: " + target.getString());
+		System.out.println("--String of target: " + target.getStr());
 		System.out.println("--String of obj: " + target.getObj().toString());
 		System.out.println("----------------------------------------");
 
-		target = (Prototype) source.deepClone();
+		target = (ProtoType) source.deepClone();
 		System.out.println("Deep Clone:");
 		System.out.println("Class ID of target: " + target.toString());
-		System.out.println("--String of target: " + target.getString());
+		System.out.println("--String of target: " + target.getStr());
 		System.out.println("--String of obj: " + target.getObj().toString());
 		System.out.println("----------------------------------------");
 	}
