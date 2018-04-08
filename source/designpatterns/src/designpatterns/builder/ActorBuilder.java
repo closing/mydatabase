2df9692adbc11e0c71a3f3d8f13c40ffe046cefa
closing <zhu.yuxing@hotmail.com>
2018-04-08 17:42:52 +0900

    Create ActorBuilder.java

---
+++ source/designpatterns/src/designpatterns/builder/ActorBuilder.java
@@ -0,0 +1,20 @@
+package designpatterns.builder;
+
+public abstract class ActorBuilder {
+	protected Actor actor = new Actor();
+
+	public abstract void buildType();
+
+	public abstract void buildSex();
+
+	public abstract void buildFace();
+
+	public abstract void buildCostume();
+
+	public abstract void buildHairstyle();
+
+	public Actor createActor() {
+		return actor;
+	}
+
+}
