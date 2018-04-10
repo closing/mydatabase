cb492f1543e4d7abd3e6afb99bf884ab8ee6b9d8
closing <zhu.yuxing@hotmail.com>
2018-04-10 13:35:56 +0900

    Create SalaryServerServlet.java

---
+++ source/javaservletprogramming/src/chr08/SalaryServerServlet.java
@@ -0,0 +1,20 @@
+package chr08;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SalaryServerServlet extends HttpServlet {
+
+  public void doGet(HttpServletRequest req, HttpServletResponse res)
+                               throws ServletException, IOException {
+    res.setContentType("text/plain");
+    PrintWriter out = res.getWriter();
+
+    out.println("Top-secret information:");
+    out.println("Everyone else gets paid more than you!");
+  }
+}
