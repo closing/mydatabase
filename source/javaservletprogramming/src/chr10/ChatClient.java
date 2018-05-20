688aaecf9c26cbe13f0bbd0e320b4dba890529de
closing <zhu.yuxing@hotmail.com>
2018-05-20 16:06:32 +0900

    Add files via upload

---
+++ source/javaservletprogramming/src/chr10/ChatClient.java
@@ -0,0 +1,8 @@
+package chr10;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ChatClient extends Remote {
+    public void setNextMessage(String message) throws RemoteException;
+}
